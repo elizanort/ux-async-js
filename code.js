227c607cf3d1f8613844945a2b28f35de12e5e94
“elizanort” <elizabethnortman@gmail.com>
2020-05-12 11:49:56 -0400

    first item in results does not return a githubUser.html_url

--- code.js
+++ code.js
@@ -3,7 +3,8 @@ window.onload = function () { // Run this once the page has loaded.
     document.querySelector("#searchButton").addEventListener("click", searchGithub);
     function searchGithub() {
         const searchUserText = document.querySelector("#searchUser").value;
-        fetch("https://api.github.com/search/users?q=" + searchUserText).then((response) => response.json())
+        fetch("https://api.github.com/search/users?q=" + searchUserText)
+        .then((response) => response.json())
         .then(result => {
             let githubUsers = result.items;
             renderUserList(githubUsers);
@@ -19,8 +20,9 @@ window.onload = function () { // Run this once the page has loaded.
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
             html += "<li>";
-            html += `<strong>${githubUser.login}</strong>`;
-            html += `<a target="_blank" href="${githubUser.html_url}">`;
+            html += `<img src=${githubUser.avatar_url} class="user_avatar"></img>`
+            html += `<strong >${githubUser.login}</strong>`;
+            html += `<a target="_blank" class="username_link" href="${githubUser.html_url}">`;
             html += "</li>";
         }
         html += "</ul>";
